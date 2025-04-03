59538335c7bd350d51fc70cf3dbc335fa2de24a9
vaibhav kandwal <19517445+boxdox@users.noreply.github.com>
2025-04-03 10:20:13 +0530

    fix: run failure

--- src/fetchStuff.ts
+++ src/fetchStuff.ts
@@ -20,10 +20,10 @@ const learningData = await fetcher<IGithubCard[]>(GITHUB_API_LEARNING_LIST, GITH
 const projectsData = await fetcher<IGithubCard[]>(GITHUB_API_PROJECTS_LIST, GITHUB_HEADERS)
 
 // Join the list of items as `a, b, c and d`
-export const learningList = learningData
+export const learningList = (learningData ?? [])
   .map(({ note }) => note)
   .reduce((a, b, i, arr) => (i <= arr.length - 2 ? `${a}, ${b}` : `${a} and ${b}`))
 
-export const projectsList = projectsData.map(({ note }) => `- ${note}`).join('\n')
+export const projectsList = (projectsData ?? []).map(({ note }) => `- ${note}`).join('\n')
 
 export const today = updatedDate()
